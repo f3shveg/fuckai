2664461fc690f353d25ef6b2a447c84aeec9ad30
mike <qwertykek2004@gmail.com>
2024-08-19 17:00:33 +0300

    Added keyword extraction functionality using Keywords module and measured execution time

--- Modules/Keywords.py
+++ Modules/Keywords.py
@@ -1,11 +1,30 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
 
-def keywords(test_text):
+try:
+    with open("Modules/keyword_list.txt", "r") as keyword_file:
+        lines = keyword_file.readlines()
+        keywords = []
+        for line in lines:
+            line = line.strip()  # remove newline character
+            if line:  # check if line is not empty
+                keyword_list = line.split(", ")
+                if keyword_list:  # check if keyword_list is not empty
+                    keywords.append(keyword_list[0])
+except FileNotFoundError:
+    print("The file does not exist")
+except Exception as e:
+    print("An error occurred: ", str(e))
+      
+keywords = [keyword.strip('"') for keyword in keywords]       
+
+    
+def keywordss(test_text):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli")
     sequence_to_classify = test_text
-    candidate_labels = ["хорошо", "плохо", "war", "животные", "technology"]
-    output = classifier(sequence_to_classify, candidate_labels, multi_label=False,)
+    candidate_labels = keywords
+    output = classifier(sequence_to_classify, candidate_labels, multi_label=True,)
+    print(output)
 
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import Modules.Note_modules as Note_modules
 from Modules.Note_modules import CreateNote, WriteToNote, RenameNote, ChangeLinks, NoteConnection
 import Modules.Summarization_module as Sum_module
+import Modules.Keywords as Keywords
+from datetime import datetime
+start=datetime.now()
 
+Keywords.keywordss("""Российские военные отразили три штурмовые атаки в направлении сел Ольговка, Русское и Черкасское Поречное. Там войска противника потеряли танк, боевую машину пехоты, две боевые бронированные машины и более 25 человек. Четырех украинских военнослужащих взяли в плен, отметили в Минобороны.""")
 
-RenameNote("1233", "1234")
+print(datetime.now()-start)
